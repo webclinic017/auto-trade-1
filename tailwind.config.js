b48bc3592b97e2a371a4b8efb825e62dcb12fc49
rohit20001221 <rohit20001221@gmail.com>
2021-07-13 00:52:09 +0530

    remove pruge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  pruge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
