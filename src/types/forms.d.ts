6e22ec811f2f2d4272788e18ab27e38f0de05156
rohit20001221 <rohit20001221@gmail.com>
2022-01-17 22:54:52 +0530

    feat (settings): fixing the settings form

--- src/types/forms.d.ts
+++ src/types/forms.d.ts
@@ -1,6 +1,8 @@
 import { UserProfile } from "./user";
 
-export type UserProfileUpdateForm = Omit<
-  UserProfile,
-  "id" | "is_accesstoken_valid" | "user" | "zerodha_last_login"
+export type UserProfileUpdateForm = Partial<
+  Omit<
+    UserProfile,
+    "id" | "is_accesstoken_valid" | "user" | "zerodha_last_login"
+  >
 >;
