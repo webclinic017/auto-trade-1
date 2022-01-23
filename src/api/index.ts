f1bb2660b1f9ea83552437c3153f1fb413805d58
rohit20001221 <rohit20001221@gmail.com>
2022-01-23 16:37:45 +0530

    refactor(code): code cleaning

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,18 +1,8 @@
 import axios from "axios";
 import { QueryClient } from "react-query";
 
-const uri = process.env.REACT_APP_API_URI;
-const wsprotocol = process.env.REACT_APP_API_WS_PROTOCOL;
-
 export const sse_uri =
   process.env.REACT_APP_API_HOST + "/events/notifier/trade" ?? "";
-
-export const ws = {
-  ws: `${wsprotocol}://` + uri,
-  socket: `${wsprotocol}://` + uri + "/ws/indian/trade",
-  users: `${wsprotocol}://` + uri + "/ws/users",
-  orders: `${wsprotocol}://` + uri + "/ws/orders",
-};
 
 export const Axios = axios.create({
   baseURL: process.env.REACT_APP_API_HOST,
--- src/services/ws.js
+++
@@ -1,5 +0,0 @@
-import { ws } from "../api";
-
-export const socket = new WebSocket(ws.socket);
-export const sockuser = new WebSocket(ws.users);
-export const orders = new WebSocket(ws.orders);
